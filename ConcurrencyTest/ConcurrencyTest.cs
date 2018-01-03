b0fa4f2a8e431ad0ac7ffa975380e6fb2925e1aa
Zabeen Patel <Zabeen.Patel@anthonynolan.org>
2018-01-03 16:27:19 +0000

    New project with exercise code

---
+++ ConcurrencyTest/ConcurrencyTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+
+namespace ConcurrencyTest
+{
+    public class ConcurrencyTest
+    {
+        private class DataStore
+        {
+            public int Value { get; set; }
+        }
+
+        private DataStore store = new DataStore();
+
+        public void TestConcurrency()
+        {
+            var thread1 = new Thread(IncrementTheValue);
+            var thread2 = new Thread(IncrementTheValue);
+
+            thread1.Start();
+            thread2.Start();
+
+            thread1.Join();
+            thread2.Join();
+
+            Console.WriteLine($"Final value: {store.Value}");
+        }
+
+        private void IncrementTheValue()
+        {
+            lock (store)
+            {
+                store.Value++;
+            }
+        }
+    }
+}
---
+++ ConcurrencyTest/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConcurrencyTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (var i = 1; i < 101; i++)
+            {
+                Console.Write($"{i}: ");
+                new ConcurrencyTest().TestConcurrency();
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
